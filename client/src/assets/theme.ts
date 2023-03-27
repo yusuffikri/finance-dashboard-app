537d565705b30e434dcefd8dbcdc81da7a70f51a
yusuffikri <yusuffiks99@gmail.com>
2023-03-28 00:16:25 +0800

    mui configuration

--- client/src/assets/theme.ts
+++ client/src/assets/theme.ts
@@ -69,36 +69,36 @@ export const tokens = {
       },
     },
     typography: {
-      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontFamily: ["Poppins", "sans-serif"].join(","),
       fontSize: 12,
       h1: {
-        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 32,
       },
       h2: {
-        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 24,
       },
       h3: {
-        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 20,
         fontWeight: 800,
         color: tokens.grey[200],
       },
       h4: {
-        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 14,
         fontWeight: 600,
         color: tokens.grey[300],
       },
       h5: {
-        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 12,
         fontWeight: 400,
         color: tokens.grey[500],
       },
       h6: {
-        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 10,
         color: tokens.grey[700],
       },
