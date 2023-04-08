5f0fb3f19b0e825d2bee0fa3ca27e9e83e17a627
yusuffikri <yusuffiks99@gmail.com>
2023-04-08 16:30:57 +0700

    react router & navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { CssBaseline, ThemeProvider } from "@mui/material"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "@/sections/navbar"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -12,6 +13,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline/>
       <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+        <Navbar />
         <Routes>
           <Route path="/" element={<div>Dashboard Page</div>}/>
           <Route element={<div>Prediction Page</div>}/>
